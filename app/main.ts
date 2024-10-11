d5d64b6f86fa17c23d50edd3063a576cd2ec86fa
hsmyc <hllsmtyc@gmail.com>
2024-10-11 13:54:46 +0200

    i don't know what i'm doing

--- app/main.ts
+++ app/main.ts
@@ -1,37 +1,36 @@
-import O from "./core/observer";
+import { createState, createComputedState } from "./core/observer";
 
-const value = O(0);
-
+const [val, setval, subval] = createState(0);
+const [_, subValC] = createComputedState(() => val() * 2);
 const cEl = document.getElementById("count");
+const sCel = document.getElementById("scount");
 const iEl = document.getElementById("increment");
 const dEl = document.getElementById("decrement");
-const dataEl = document.getElementById("data");
+const diEl = document.getElementById("dincrement");
 
 function increment() {
-  value.set(value.get() + 1);
+  setval(val() + 1);
+}
+
+function doubleIncrement(value: any, setVal: any) {
+  setVal(value() + 2);
 }
 
 function decrement() {
-  value.set(value.get() - 1);
+  setval(val() - 1);
 }
-async function getResource() {
-  fetch("https://jsonplaceholder.typicode.com/posts/1")
-    .then((response) => response.json())
-    .then((data) => {
-      value.set(data.body);
-    });
-}
+
 iEl?.addEventListener("click", increment);
+diEl?.addEventListener("click", () => doubleIncrement(val, setval));
 dEl?.addEventListener("click", decrement);
-dataEl?.addEventListener("click", getResource);
 
 function render(v: any) {
   cEl!.innerText = v;
 }
 
 function renderData(v: any) {
-  dataEl!.innerText = v;
+  sCel!.innerText = v;
 }
 
-value.subscribe(render);
-value.subscribe(renderData);
+subval(render);
+subValC(renderData);
