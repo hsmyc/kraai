38379cd5a857890731048d5dce23e8fa24ddc845
hsmyc <hllsmtyc@gmail.com>
2024-10-11 20:33:06 +0200

    few examples added

--- app/main.ts
+++ app/main.ts
@@ -3,10 +3,12 @@ import {
   createComputedState,
   createHybridState,
 } from "./core/statemanager";
+
+// States
 const [val, setval, subval] = createState(0);
 const [_, subValC] = createComputedState(() => val() * 2);
-
-const [obj, setObj, subObj] = createHybridState(
+const [t, setT, subT] = createState(0);
+const [_2, setObj, subObj] = createHybridState(
   () => ({
     no: val(),
     name: {
@@ -22,13 +24,20 @@ const [obj, setObj, subObj] = createHybridState(
     },
   }
 );
+const [_3, setData, subData] = createState({});
+
+// DOM Elements
 const cEl = document.getElementById("count");
 const sCel = document.getElementById("scount");
 const dCel = document.getElementById("dcount");
 const iEl = document.getElementById("increment");
 const dEl = document.getElementById("decrement");
-const diEl = document.getElementById("dincrement");
+const diEl = document.getElementById("name");
+const tEL = document.getElementById("timer");
+const dataEl = document.getElementById("data");
+const dataEl2 = document.getElementById("data2");
 
+// Event Handlers
 function increment() {
   setval(val() + 1);
 }
@@ -36,6 +45,7 @@ function increment() {
 function decrement() {
   setval(val() - 1);
 }
+
 function setObject() {
   setObj({
     name: {
@@ -44,12 +54,30 @@ function setObject() {
     },
   });
 }
+
+function timer() {
+  setT(t() + 1);
+}
+
+function getData() {
+  fetch("https://randomuser.me/api/")
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data.results[0]);
+      setData(data.results[0]);
+    });
+}
+
+// Event Listeners
 iEl?.addEventListener("click", increment);
 diEl?.addEventListener("click", () => {
   setObject();
 });
 dEl?.addEventListener("click", decrement);
+dataEl?.addEventListener("click", getData);
+setInterval(timer, 1000);
 
+// Renderers
 function render(v: any) {
   cEl!.innerText = v;
 }
@@ -62,6 +90,17 @@ function renderData2(v: any) {
   dCel!.innerText = `${v.no} ${v.name.first} ${v.name.last}`;
 }
 
+function renderTimer(v: any) {
+  tEL!.innerText = v;
+}
+
+function renderData3(v: any) {
+  dataEl2!.innerText = JSON.stringify(v);
+}
+
+// Subscribers
 subval(render);
 subValC(renderData);
 subObj(renderData2);
+subT(renderTimer);
+subData(renderData3);
